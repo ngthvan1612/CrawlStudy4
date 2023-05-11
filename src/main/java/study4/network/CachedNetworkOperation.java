aff216d024db3e884f74186785fc9243f5071b35
Nguyễn Thành Văn <ngthvan1612@gmail.com>
2023-05-12 00:40:31 +0700

    Update CachedNetworkOperation.java

--- src/main/java/study4/network/CachedNetworkOperation.java
+++ src/main/java/study4/network/CachedNetworkOperation.java
@@ -17,7 +17,7 @@ public class CachedNetworkOperation {
     private final static Jedis jedisInstance = new Jedis();
     private final static JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
     static {
-        jedisPoolConfig.setMaxTotal(2);
+        jedisPoolConfig.setMaxTotal(1024);
     }
     private final static JedisPool jedisPool = new JedisPool(jedisPoolConfig);
 
