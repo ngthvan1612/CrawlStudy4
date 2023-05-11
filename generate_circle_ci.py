af3b38ca53ab40778929f5fb7b7ad2b2c929756c
Nguyễn Thành Văn <ngthvan1612@gmail.com>
2023-05-12 01:36:45 +0700

    w

--- generate_circle_ci.py
+++ generate_circle_ci.py
@@ -33,7 +33,7 @@ JOB_BASE = """
             mvn test-compile compile
             mvn exec:java -Djava.util.concurrent.ForkJoinPool.common.parallelism=32
     store_artifacts:
-      path: .
+      path: ./
 """
 
 FOOTER = """
